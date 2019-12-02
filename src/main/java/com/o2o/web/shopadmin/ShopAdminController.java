f277614d796ff7fb63e36b6af0de54ff7783c5e6
Dell <2323333@qq.com>
2019-12-03 01:08:27 +0800

    git push

--- src/main/java/com/o2o/web/shopadmin/ShopAdminController.java
+++ src/main/java/com/o2o/web/shopadmin/ShopAdminController.java
@@ -7,18 +7,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "shopadmin", method = {RequestMethod.GET})
 public class ShopAdminController {
+    // 转发至注册/编辑界面
     @RequestMapping(value = "/shopoperation")
     public String shopOperation(){
        return "shop/shopoperation";
     }
 
+    // 转发至店铺列表页面
     @RequestMapping(value = "/shoplist")
     public String shopList(){
         return "shop/shoplist";
     }
 
+    // 转发至电偶管理页面
     @RequestMapping(value = "/shopmanagement")
     public String shopManagement(){
         return "shop/shopmanagement";
+    }
+
+    // 转发至商品类别管理页面
+    @RequestMapping(value = "/productcategorymanagement", method = RequestMethod.GET)
+    private String productCategoryManage() {
+        return "shop/productcategorymanagement";
     }
 }
